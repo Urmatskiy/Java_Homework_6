7b32438a6471d525ad8717549b4fc9ddcca6eacc
Urmatskiy <nickiton14@gmail.com>
2023-04-15 21:59:51 +0300

    Изменил экземпляр в main

--- progrma.java
+++ progrma.java
@@ -2,7 +2,7 @@ package Homework_6;
 
 public class progrma {
     public static void main(String[] args) {
-        Employee worker = new Employee(34, "Alex", "James");
+        Employee worker = new Employee(1, 34, "Alex", "James");
 
         System.out.println(worker.toString());
         
