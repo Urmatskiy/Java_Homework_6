f9d58e5875551bf276760600f4bb7a58549b4401
Urmatskiy <nickiton14@gmail.com>
2023-04-15 10:50:43 +0300

    Создал два конструктора

--- Employee.java
+++ Employee.java
@@ -2,4 +2,24 @@ package Homework_6;
 
 public class Employee {
     
+    int age;
+    String firstName;
+    String secondName;
+    double salary;
+
+    public Employee(int age, String firstName, String secondName, double salary) {
+        this.age = age;
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.salary = salary;
+    }
+
+    public Employee(int age, String firstName, String secondName) {
+        this.age = age;
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.salary = 50.0;
+    }
+
+
 }
