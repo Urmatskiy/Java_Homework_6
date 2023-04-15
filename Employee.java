f599d2c103b7fa6926746e9a145c62a4d5b90d7d
Urmatskiy <nickiton14@gmail.com>
2023-04-15 10:40:13 +0300

    Создал репозиторий

---
+++ Employee.java
@@ -0,0 +1,5 @@
+package Homework_6;
+
+public class Employee {
+    
+}
---
+++ progrma.java
@@ -0,0 +1,7 @@
+package Homework_6;
+
+public class progrma {
+    public static void main(String[] args) {
+        
+    }
+}
